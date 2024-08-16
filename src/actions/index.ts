3eef49303530709676f9aa2cfff946a2b789c352
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-16 21:35:42 +0630

    auth function added

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,5 @@
 "use server";
 
-import * as auth from "@/auth";
-
-export const signIn = async () => {
-  return auth.signIn("github");
-};
-export const signOut = async () => {
-  return auth.signOut();
-};
+import { createComment } from "./create-comment";
+import { createPost } from "./create-post";
+import { createTopic } from "./create-topic";
