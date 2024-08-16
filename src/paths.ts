98e9d5b43a104b505ebdd61976472284203df50c
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-16 21:23:14 +0630

    add path helper function;

---
+++ src/paths.ts
@@ -0,0 +1,14 @@
+const paths = {
+  home() {
+    return "/";
+  },
+  topic(topicSlung: string) {
+    return `/topics/${topicSlung}`;
+  },
+  postCreate(topicSlung: string) {
+    return `/topics/${topicSlung}/posts/new`;
+  },
+  postShow(topicSlung: string, postId: number) {
+    return `/topics/${topicSlung}/posts/${postId}`;
+  },
+};
