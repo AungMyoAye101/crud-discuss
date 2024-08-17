18b1bf82005c6a5213195aafbd5fc0c5a6319bd9
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-17 21:02:10 +0630

    add zod package

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,7 +1,24 @@
 "use server";
 
+import { title } from "process";
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase leter",
+    }),
+  description: z.string().min(10),
+});
 export const createTopic = async (formData: FormData) => {
-  const title = formData.get("title");
-  const description = formData.get("description");
-  console.log(title, description);
+  const result = createTopicSchema.safeParse({
+    title: formData.get("title"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error);
+  }
 };
